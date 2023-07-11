a4534ef0466d685b78b4c59a6311a2de7ecc0dcf
adithk <adithk1308@gmail.com>
2023-07-11 14:12:43 +0530

    homepage->login->dashboard->logout->dashboard sessions are handled properly

---
+++ src/main/java/com/adith/loginpage/configuration/WebSecurityConfiguration.java
@@ -0,0 +1,30 @@
+package com.adith.loginpage.configuration;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfiguration {
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin((form) -> form
+                        .loginPage("/login").defaultSuccessUrl("/dashboard")
+                        .permitAll()
+                )
+                .logout((logout) -> logout.deleteCookies("JSESSIONID").permitAll());
+
+        return http.build();
+    }
+}
